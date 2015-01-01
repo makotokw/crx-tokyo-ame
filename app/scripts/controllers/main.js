b7211d8d805c8b231b9427b7c79cfbb38b4b804e
makoto_kw <makoto.kw@gmail.com>
2015-01-01 19:32:21 +0900

    Adjust initial layout and focus

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,12 +5,4 @@ angular.module('tokyoAmeApp')
     $scope.scale = Amesh.scales.tokyo;
     $scope.recordedDate =
     $scope.updatedDate = Amesh.getLatestMeasurementDateTime();
-
-    $scope.init = function() {
-      console.log('MainCtrl.init');
-      // hack! diasble auto focus
-      $timeout(function () {
-        document.activeElement.blur();
-      }, 100);
-    }
   });
