1b6a7be17c7837977c794288918e6dd03613c235
makoto_kw <makoto.kw@gmail.com>
2015-01-14 23:40:30 +0900

    i18n

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,20 @@
 
 angular.module('tokyoAmeApp')
   .controller('MainCtrl', function ($scope, $timeout, Amesh, Options) {
+
+    function i18n(messageName) {
+      return chrome.i18n.getMessage(messageName);
+    }
+
     $scope.scale = Options.getMapScale() || Amesh.scales.tokyo;
     $scope.recordedDate =
     $scope.updatedDate = Amesh.getLatestMeasurementDateTime();
 
+    $scope.recordedDateFormat = i18n('recordedDateFormat');
+    $scope.updatedDateFormat = i18n('updatedDateFormat');
+
+    $scope.i18n = i18n;
     $scope.close = function() {
       window.close();
-    }
+    };
   });
